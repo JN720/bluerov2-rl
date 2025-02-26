5f3da33e8c58e224f75c1167b3fd4c709cd0f14c
JN720 <jsnxyz695@gmail.com>
2025-02-25 20:06:06 -0800

    added rosdep library files

---
+++ train_launch.py
@@ -0,0 +1,26 @@
+from simple_launch import SimpleLauncher, GazeboBridge
+
+
+def generate_launch_description():
+    
+    sl = SimpleLauncher()
+    sl.declare_arg('gui', default_value=True)
+    sl.declare_arg('spawn', default_value=True)
+
+    with sl.group(if_arg='gui'):
+        sl.gz_launch(sl.find('bluerov2_description', 'training_world.sdf'), "-r")
+        
+    with sl.group(unless_arg='gui'):
+        sl.gz_launch(sl.find('bluerov2_description', 'training_world.sdf'), "-r -s")
+
+    bridges = [GazeboBridge.clock(),
+            #    GazeboBridge('/world/ocean/set_pose', '/set_pose', 'geometry_msgs/Pose', GazeboBridge.ros2gz),
+               GazeboBridge('/ocean_current', '/current', 'geometry_msgs/Vector3',
+                            GazeboBridge.ros2gz)]
+        
+    sl.create_gz_bridge(bridges)
+
+    with sl.group(if_arg='spawn'):
+        sl.include('bluerov2_description', 'upload_bluerov2_launch.py')
+        
+    return sl.launch_description()
